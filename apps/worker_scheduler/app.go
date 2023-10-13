3372ae07913220d34ddbedb1cda68a80f3779643
adriansyah <adriansyah.azhari@kelaspintar.id>
2023-10-13 11:36:21 +0700

    add scheduler

--- apps/worker_scheduler/app.go
+++ apps/worker_scheduler/app.go
@@ -36,27 +36,11 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	// // You can use cron spec string to specify the schedule.
-	// entryID, err := scheduler.Register("* * * * *", task)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Printf("registered an entry: %q\n", entryID)
-
-	// You can use "@every <duration>" to specify the interval.
-
-	entryID, err := scheduler.Register("@every 30s", task)
+	entryID, err := scheduler.Register("0 0 * * *", task)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("registered an entry: %q\n", entryID)
-
-	// You can also pass options.
-	// entryID, err := scheduler.Register("0 0 * * *", task, asynq.Queue("default"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Printf("registered an entry: %q\n", entryID)
 
 	if err := scheduler.Run(); err != nil {
 		log.Fatal(err)
